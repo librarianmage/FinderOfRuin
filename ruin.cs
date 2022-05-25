4a6388d811f98b948576e53b20b5a83aef8da32e
Ray Arndorfer <raymond.arndorfer@gmail.com>
2022-05-25 04:31:22 -0500

    Stabilize to version 1.0
    Simplify Harmony patch code
    Update mod metadata

--- ruin.cs
+++ ruin.cs
@@ -6,15 +6,12 @@ namespace FinderOfRuin.HarmonyPatches
     [HarmonyPatch(typeof(XRL.LoreGenerator),nameof(XRL.LoreGenerator.RuinOfHouseIsnerLore))]
     class LorePatcher
     {
-        static string Postfix(string __result)
+        static string Postfix(string lore)
         {
-            string lore;
-            lore = __result
+            return lore
                 .Replace("masterwork", "&cmasterwork&y")
                 .Replace("Ruin", "&rRuin&y")
                 .Replace("House Isner", "&MHouse Isner&y");
-            UnityEngine.Debug.Log($"Found the pistol! {lore}");
-            return lore;
         }
     }
 }
