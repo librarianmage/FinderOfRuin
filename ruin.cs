652b9d26d7fdadcee39667556fc0382f5d2c3137
librarianmage <raymond.arndorfer@gmail.com>
2022-07-24 12:04:55 +0000

    Add additional capitalization options

--- ruin.cs
+++ ruin.cs
@@ -1,4 +1,6 @@
 using HarmonyLib;
+using System;
+using System.Text.RegularExpressions;
 using XRL;
 using XRL.UI;
 
@@ -8,24 +10,45 @@ namespace FinderOfRuin.HarmonyPatches
     [HarmonyPatch(nameof(LoreGenerator.RuinOfHouseIsnerLore))]
     class LorePatcher
     {
-        static void Postfix(ref string lore)
+        static void Postfix(ref string __result)
         {
+            String Color = Options.GetOption("Books_FinderOfRuin_Color", "No");
+            String Capitalization = Options.GetOption("Books_FinderOfRuin_Capitalization", "Default");
 
-            if (Options.GetOption("Books_FinderOfRuin_Color", "No") == "Yes")
+            if (Capitalization == "Key Words")
             {
-                lore = lore
-                    .Replace("masterwork", "&cmasterwork&y")
-                    .Replace("Ruin", "&rRuin&y")
-                    .Replace("House Isner", "&MHouse Isner&y");
-            }
-            if (Options.GetOption("Books_FinderOfRuin_Capitalization", "No") == "Yes")
-            {
-                lore = lore
+                __result = __result
                     .Replace("masterwork", "MASTERWORK")
                     .Replace("Ruin", "RUIN")
                     .Replace("House Isner", "HOUSE ISNER");
             }
+            else if (Capitalization == "Entire Clue") {
+                Regex rx = new Regex(@"\{(.*)\}");
+                MatchEvaluator evaluator = new MatchEvaluator(Capitalize);
+                __result = rx.Replace(__result, evaluator);
+            }
+
+            if (Color == "Yes")
+            {
+                if (Capitalization == "Default")
+                {
+                    __result = __result
+                        .Replace("masterwork", "&cmasterwork&y")
+                        .Replace("Ruin", "&rRuin&y")
+                        .Replace("House Isner", "&MHouse Isner&y");
+                }
+                else
+                {
+                    __result = __result
+                        .Replace("MASTERWORK", "&cMASTERWORK&y")
+                        .Replace("RUIN", "&rRUIN&y")
+                        .Replace("HOUSE ISNER", "&MHOUSE ISNER&y");
+                }
+            }
+
 
         }
+
+        static string Capitalize(Match m) => m.Captures[0].Value.ToUpper();
     }
 }
