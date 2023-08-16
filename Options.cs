3317237853d4bd7e60d3a5865ba64b8b2722e877
librarianmage <raymond.arndorfer@gmail.com>
2023-08-16 03:31:24 +0000

    Add tentative format fix patch
    FossilOrigin-Name: be0030c167f8fb75fcfa5945abf97cebc6c8d8d5e04efc27d5dc7315f0c00ae0

--- Options.cs
+++ Options.cs
@@ -1,7 +1,86 @@
+using System;
+using static XRL.UI.Options;
+
 namespace FinderOfRuin
 {
-    // static class Options
-    // {
-    //     public enum
-    // }
+    public static class Options
+    {
+        public static string Option(string name) => $"Books_FinderOfRuin_{name}";
+
+        public static class Formatting
+        {
+            public static bool Enabled => GetOption(Option("EnableFormatting"), "Yes").EqualsNoCase("Yes");
+
+            public static bool Wanted => Enabled && (Highlight.Enabled || Capitalization.Enabled);
+
+            public static class Highlight
+            {
+                public static bool Enabled => GetOption(Option("EnableHighlight"), "Yes").EqualsNoCase("Yes");
+
+                public enum HighlightSpan { KeyWords, EntireClue };
+
+                public static HighlightSpan Span {
+                    get {
+                        var span = GetOption(Option("Highlight"), "Key Words");
+
+                        if (Enum.TryParse<HighlightSpan>(span.Replace(" ", ""), out var result))
+                        {
+                            return result;
+                        }
+                        else
+                        {
+                            return HighlightSpan.KeyWords;
+                        }
+                    }
+                }
+
+                public enum HighlightStyle { AllWhite, ColoredKeyWords };
+
+                public static HighlightStyle Style
+                {
+                    get
+                    {
+                        var style = GetOption(Option("HighlightStyle"), "Key Words");
+
+                        if (Enum.TryParse<HighlightStyle>(style.Replace(" ", ""), out var result))
+                        {
+                            return result;
+                        }
+                        else
+                        {
+                            return HighlightStyle.ColoredKeyWords;
+                        }
+                    }
+                }
+            }
+
+            public static class Capitalization
+            {
+                public static bool Enabled => GetOption(Option("EnableCapitalization"), "Yes").EqualsNoCase("Yes");
+
+                public enum CapitalizationSpan { KeyWords, EntireClue };
+
+                public static CapitalizationSpan Span
+                {
+                    get {
+                        var span = GetOption(Option("Highlight"), "Key Words");
+
+                        if (Enum.TryParse<CapitalizationSpan>(span.Replace(" ", ""), out var result))
+                        {
+                            return result;
+                        }
+                        else
+                        {
+                            return CapitalizationSpan.KeyWords;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static class SecretSmuggler
+        {
+            public static bool Enabled => GetOption(Option("EnableSmuggling"), "Yes").EqualsNoCase("Yes");
+        }
+    }
 }
