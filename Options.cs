5ac1f455d6939f01b221f448397e0e943a106e39
librarianmage <raymond.arndorfer@gmail.com>
2024-06-10 06:00:24 +0000

    Formatting
    FossilOrigin-Name: 0fe51a39a4411e8968b10668d355fd8f59314e27f03f030322bc6d5882558547

--- Options.cs
+++ Options.cs
@@ -1,89 +1,76 @@
-using System;
 using static XRL.UI.Options;
 
 namespace FinderOfRuin
 {
     public static class Options
     {
-        public static string Option(string name) => $"Books_FinderOfRuin_{name}";
+        private static string Option(string name) => $"Books_FinderOfRuin_{name}";
 
         /// Hewwo
         public static class Formatting
         {
-            public static bool Enabled => GetOption(Option("EnableFormatting"), "Yes").EqualsNoCase("Yes");
+            public static bool Enabled =>
+                GetOption(Option("EnableFormatting"), "Yes").EqualsNoCase("Yes");
 
             public static bool Wanted => Enabled && (Highlight.Enabled || Capitalization.Enabled);
 
-            public static bool WantKeyWords => Enabled && (Highlight.WantKeyWords || Capitalization.WantKeyWords);
+            public static bool WantKeyWords =>
+                Enabled && (Highlight.WantKeyWords || Capitalization.WantKeyWords);
 
-            public static bool WantEntireClue => Enabled && (Highlight.WantEntireClue || Capitalization.WantEntireClue);
+            public static bool WantEntireClue =>
+                Enabled && (Highlight.WantEntireClue || Capitalization.WantEntireClue);
 
-            public enum Span { KeyWords, EntireClue };
+            public enum Span
+            {
+                KeyWords,
+                EntireClue
+            };
 
             public static class Highlight
             {
-                public static bool Enabled => GetOption(Option("EnableHighlight"), "Yes").EqualsNoCase("Yes");
+                public static bool Enabled =>
+                    GetOption(Option("EnableHighlight"), "Yes").EqualsNoCase("Yes");
 
-                public static Span Span {
-                    get {
-                        var span = GetOption(Option("Highlight"), "Key Words");
-
-                        if (Enum.TryParse<Span>(span.Replace(" ", ""), out var result))
-                        {
-                            return result;
-                        }
-                        else
-                        {
-                            return Span.KeyWords;
-                        }
-                    }
-                }
-
-                public enum HighlightStyle { AllWhite, ColoredKeyWords };
-
-                public static HighlightStyle Style
-                {
-                    get
+                public static Span Span =>
+                    GetOption(Option("Highlight"), "Key Words") switch
                     {
-                        var style = GetOption(Option("HighlightStyle"), "Key Words");
+                        "Key Words" => Span.KeyWords,
+                        "Entire Clue" => Span.EntireClue,
+                        _ => Span.KeyWords
+                    };
 
-                        if (Enum.TryParse<HighlightStyle>(style.Replace(" ", ""), out var result))
-                        {
-                            return result;
-                        }
-                        else
-                        {
-                            return HighlightStyle.ColoredKeyWords;
-                        }
-                    }
-                }
+                public enum HighlightStyle
+                {
+                    AllWhite,
+                    ColoredKeyWords
+                };
 
-                public static bool WantKeyWords => Enabled
-                    && (Span == Span.KeyWords
-                        || Style == HighlightStyle.ColoredKeyWords);
+                public static HighlightStyle Style =>
+                    GetOption(Option("HighlightStyle"), "Colored Key Words") switch
+                    {
+                        "All White" => HighlightStyle.AllWhite,
+                        "Colored Key Words" => HighlightStyle.ColoredKeyWords,
+                        _ => HighlightStyle.ColoredKeyWords
+                    };
+
+                public static bool WantKeyWords =>
+                    Enabled && (Span == Span.KeyWords || Style == HighlightStyle.ColoredKeyWords);
 
                 public static bool WantEntireClue => Enabled && Span == Span.EntireClue;
             }
 
             public static class Capitalization
             {
-                public static bool Enabled => GetOption(Option("EnableCapitalization"), "Yes").EqualsNoCase("Yes");
+                public static bool Enabled =>
+                    GetOption(Option("EnableCapitalization"), "Yes").EqualsNoCase("Yes");
 
-                public static Span Span
-                {
-                    get {
-                        var span = GetOption(Option("Highlight"), "Key Words");
-
-                        if (Enum.TryParse<Span>(span.Replace(" ", ""), out var result))
-                        {
-                            return result;
-                        }
-                        else
-                        {
-                            return Span.KeyWords;
-                        }
-                    }
-                }
+                public static Span Span =>
+                    GetOption(Option("Capitalization"), "Key Words") switch
+                    {
+                        "Key Words" => Span.KeyWords,
+                        "Entire Clue" => Span.EntireClue,
+                        _ => Span.KeyWords
+                    };
 
                 public static bool WantKeyWords => Enabled && Span == Span.KeyWords;
 
